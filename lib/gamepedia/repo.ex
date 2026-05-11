60dd98f4d84daabf7e39e0562e0f87a9b12dcee4
ResofireV2 <stryguardian@gmail.com>
2026-05-10 22:06:29 -0500

    Delete lib directory

--- lib/gamepedia/repo.ex
+++
@@ -1,5 +0,0 @@
-defmodule Gamepedia.Repo do
-  use Ecto.Repo,
-    otp_app: :gamepedia,
-    adapter: Ecto.Adapters.Postgres
-end
--- lib/gamepedia_web/controllers/health_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule GamepediaWeb.HealthController do
-  use Phoenix.Controller, formats: [:json]
-
-  def index(conn, _params) do
-    json(conn, %{status: "ok", service: "gamepedia", version: "0.1.0"})
-  end
-end
